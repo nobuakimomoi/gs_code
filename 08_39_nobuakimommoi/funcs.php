fb053f9c0589117be7c250422bd85c707a68b6ec
nobuakimomoi <nobuakimomoi@gmail.com>
2019-01-31 22:36:31 +0900

    【第８回】授業課題

---
+++ 08_39_nobuakimommoi/funcs.php
@@ -0,0 +1,26 @@
+<?php
+//共通に使う関数を記述
+
+function h($a)
+{
+    return htmlspecialchars($a, ENT_QUOTES);
+}
+
+function db_conn (){
+    try {
+        $pdo = new PDO('mysql:dbname=class07hw_db;charset=utf8;host=localhost','root','');//newする事で使える. 外部サーバーへアップロードする場合は、Localhostのところを変更
+        return $pdo;
+    } catch (PDOException $e) {
+        exit('DB-Connection-Error:'.$e->getMessage());//上記エラーの場合、動作ここでストップ。次へ行かない。
+      }      
+}
+
+function sqlerror ($stmt){
+    $error = $stmt->errorInfo();
+    exit("ErrorSQL:".$error[2]);
+  }
+
+function redirect($page){
+    header("Location: ".$page);//locationの後のスペースはマスト
+    exit;
+  }
